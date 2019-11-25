723943f5ff79592d9bd721ed99fffac2d2d5ea1b
Adam Hall <adam.hall@aligent.com.au>
2019-11-25 14:11:47 +1030

    Added Migration for resized images

--- src/Command/WebsiteImageMigrationCommand.php
+++ src/Command/WebsiteImageMigrationCommand.php
@@ -12,7 +12,6 @@
 
 namespace Aligent\S3MediaBundle\Command;
 
-
 use Aligent\S3MediaBundle\Adapter\AwsS3;
 use Aligent\S3MediaBundle\Service\FilteredAttachmentMigrationService;
 use Gaufrette\Filesystem;
@@ -26,16 +25,26 @@ use Aws\S3\S3Client;
 
 class WebsiteImageMigrationCommand extends ContainerAwareCommand
 {
-    protected const PREFIX = 'media/cache/attachment/filter';
+    protected const FILTER_PREFIX = 'media/cache/attachment/filter';
+    protected const RESIZE_PREFIX = 'media/cache/attachment/resize';
+
     protected static $defaultName = 'aligent:s3:migrate-website-images';
 
     /**
-     *
+     * @var OutputInterface\
+     */
+    protected $output;
+
+    /**
+     * Configure Command
      */
     protected function configure()
     {
-        $this->setDescription('Copies Website images to the new location required by Oro 3.1.12')
-            ->addArgument('bucket', InputArgument::REQUIRED, 'Which S3 Bucket do you want to migrate?')
+        $this
+            ->setDescription('Copies Website images to the new location required by Oro 3.1.12')
+            ->addArgument(
+                'bucket', InputArgument::REQUIRED, 'Which S3 Bucket do you want to migrate?'
+            )
             ->addArgument('key', InputArgument::REQUIRED, 'Amazon API Key')
             ->addArgument('secret', InputArgument::REQUIRED, 'Amazon API Secret')
             ->addArgument('region', InputArgument::REQUIRED, 'Amazon Bucket Region');
@@ -48,20 +57,35 @@ class WebsiteImageMigrationCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->output = $output;
+
         $s3client = new S3Client([
             'credentials' => [
-                'key'     => $input->getArgument('key'),
-                'secret'  => $input->getArgument('secret'),
+                'key'    => $input->getArgument('key'),
+                'secret' => $input->getArgument('secret'),
             ],
-            'version' => 'latest',
-            'region'  => $input->getArgument('region')
+            'version'     => 'latest',
+            'region'      => $input->getArgument('region'),
         ]);
-        
+
         $adapter = new AwsS3($s3client, $input->getArgument('bucket'));
-        
+
         /** @var FilteredAttachmentMigrationService $migrationService */
         $migrationService = $this->getContainer()->get('aligent_s3.attachment_migration_service');
 
+        $this->migrateFilteredImages($migrationService, $adapter);
+        $this->migrateResizeImages($migrationService, $adapter);
+
+        $output->writeln('Images have been successfully copied to the new destination.');
+    }
+
+    /**
+     * Migrate Website scoped filtered images
+     * @param FilteredAttachmentMigrationService $migrationService
+     * @param AwsS3 $adapter
+     */
+    protected function migrateFilteredImages($migrationService, $adapter)
+    {
         $websiteRepo = $this->getContainer()->get('doctrine')->getRepository(Website::class);
         $websites = $websiteRepo->getAllWebsites();
         $websiteManager = $this->getContainer()->get('oro_website.manager');
@@ -69,15 +93,24 @@ class WebsiteImageMigrationCommand extends ContainerAwareCommand
 
         $websiteIds = [];
         foreach ($websites as $website) {
-            $output->writeln('Migrating images for website: ' . $website->getName());
+            $this->output->writeln('Migrating images for website: ' . $website->getName());
             $websiteId = $website->getId();
             $websiteIds[] = $websiteId;
             $websiteManager->setCurrentWebsite($website);
             $configManager->setScopeId($websiteId);
-            
-            $migrationService->migrate($adapter, self::PREFIX . '/' . $websiteId, self::PREFIX);
-        }
 
-        $output->writeln('Images have been successfully copied to the new destination.');
+            $migrationService->migrate($adapter, self::FILTER_PREFIX . '/' . $websiteId, self::FILTER_PREFIX);
+        }
+    }
+
+    /**
+     * Migrate Resized Images
+     * @param FilteredAttachmentMigrationService $migrationService
+     * @param AwsS3 $adapter
+     * @return mixed
+     */
+    protected function migrateResizeImages($migrationService, $adapter)
+    {
+        return $migrationService->migrate($adapter, self::RESIZE_PREFIX, self::RESIZE_PREFIX);
     }
 }
