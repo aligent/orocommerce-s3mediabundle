fdc097011b98b51e0e763390d650ba5d11425862
Jim O'Halloran <jim.ohalloran@incore.com.au>
2025-05-22 15:39:31 +0930

    Update promise usage un SecretKeyProvider

--- src/Provider/SecretKeyCredentialProvider.php
+++ src/Provider/SecretKeyCredentialProvider.php
@@ -21,38 +21,31 @@ use GuzzleHttp\Promise\RejectedPromise;
 class SecretKeyCredentialProvider
 {
     /**
-     * @var string
-     */
-    protected $key;
-
-    /**
-     * @var string
-     */
-    protected $secret;
-
-    /**
      * SecretKeyCredentialProvider constructor.
      * @param string $key
      * @param string $secret
      */
-    public function __construct(string $key, string $secret)
-    {
-        $this->key = $key;
-        $this->secret = $secret;
+    public function __construct(
+        protected string $key,
+        protected string $secret
+    ) {
     }
 
     /**
      * @return PromiseInterface
      */
-    public function __invoke()
+    public function __invoke(): PromiseInterface
     {
         if ($this->key && $this->secret) {
-            return Promise\promise_for(
+            return Promise\Create::promiseFor(
                 new Credentials($this->key, $this->secret)
             );
         }
 
-        $msg = 'Missing amazon_s3.key or amazon_s3.secret';
-        return new RejectedPromise(new CredentialsException($msg));
+        return new RejectedPromise(
+            new CredentialsException(
+                'Missing amazon_s3.key or amazon_s3.secret'
+            )
+        );
     }
 }
